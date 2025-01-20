dfa030da2402360ea0d8317ba39843cb89155f4b
RomShemer <romshemer2000@gmail.com>
2025-01-20 11:43:46 +0200

    block register when logedin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         <div className={styles.app}>
           <header className={styles.appHeader}>
             <nav className={styles.appNav}>
-              <Link to="/" className={styles.appLink}>Register</Link>
+              {!isLoggedIn ? (<Link to="/" className={styles.appLink}>Register</Link>) : null}
               {!isLoggedIn ? ( <Link to="/login" className={styles.appLink}>Login</Link> ) : 
               ( <UserGreeting isLoggedIn={isLoggedIn} user={user} logout={logout} /> )}
             </nav>
