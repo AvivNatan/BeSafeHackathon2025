bf79db88bfbf723715ece112fc7931505bce8073
AvivNatan <aviv193193@gmail.com>
2025-01-20 12:14:16 +0200

    remove duck

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ import { connectToAllDBs } from './config/db.js';
 import userRoutes from './routes/userRoutes.js'; // מייבא את ה-routes
 import messageRoutes from './routes/messageRoutes.js';
 
-import rubberDuckRoutes from './routes/rubberDucks.js'; // Import the routes
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
